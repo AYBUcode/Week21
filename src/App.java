6a9b82cd6e471c08394d4dc077fa8ad47961139b
YEA <aybu.delivery@gmail.com>
2023-10-12 10:14:30 +0300

    print Ankara

--- src/App.java
+++ src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("TURKIYE");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ankara");
+        }
     }
 }
